4a54a5330e67c847dfd636725b05a88b08799105
illigeto <tom.illige@dematic.com>
2019-02-06 09:32:04 +0100

    finisehd /addgym

--- commands/addgym.php
+++ commands/addgym.php
@@ -1,47 +1,47 @@
-<?php 
+<?php
 // Write to log.
 debug_log('ADDGYM()');
 
 // For debug.
-//debug_log($update);
-//debug_log($data);
+// debug_log($update);
+// debug_log($data);
 
 // Get gym name.
-$input = trim(substr($update['message']['text'], 4));
-list($gym_name, $lat, $lon, $address) = explode(' ', $input);
-
+$input = trim(substr($update['message']['text'], 7));
+list ($gym_name, $lat, $lon, $address) = explode(';', $input);
+$lat = str_replace(",", ".", $lat);
+$lon = str_replace(",", ".", $lon);
 // Write to log.
 debug_log('Setting gym name to ' . $gym_name);
 
 // Private chat type.
 if ($update['message']['chat']['type'] == 'private') {
-    
+
     try {
-        
+
         global $db;
-        
+
         // Build query to check if gym is already in database or not
-        $rs = my_query(
-            "
+        $rs = my_query("
         SELECT    COUNT(*)
         FROM      gyms
-          WHERE   gym_name = '{$name}'
-         "
-        );
-        
+          WHERE   gym_name = '{$gym_name}'
+         ");
+
         $row = $rs->fetch_row();
-        
+
         // Gym already in database or new
         if (empty($row['0'])) {
             // insert gym in table.
-            debug_log('Gym not found in database gym list! Inserting gym "' . $name . '" now.');
+            debug_log('Gym not found in database gym list! Inserting gym "' . $gym_name . '" now.');
             $query = '
             INSERT INTO gyms (gym_name, lat, lon, address, show_gym)
             VALUES (:gym_name, :lat, :lon, :address, 1)
         ';
+            $message = getTranslation('gym_added');
         } else {
             // Update gyms table to reflect gym changes.
-            debug_log('Gym found in database gym list! Updating gym "' . $name . '" now.');
+            debug_log('Gym found in database gym list! Updating gym "' . $gym_name . '" now.');
             $query = '
                 UPDATE        gyms
                 SET           lat = :lat,
@@ -49,23 +49,24 @@ if ($update['message']['chat']['type'] == 'private') {
                               address = :address
                 WHERE      gym_name = :gym_name
             ';
+            $message = getTranslation('gym_updated');
         }
 
-        $statement = $dbh->prepare( $query );
+        $statement = $dbh->prepare($query);
         $statement->bindValue(':gym_name', $gym_name, PDO::PARAM_STR);
         $statement->bindValue(':lat', $lat, PDO::PARAM_STR);
         $statement->bindValue(':lon', $lon, PDO::PARAM_STR);
         $statement->bindValue(':address', $address, PDO::PARAM_STR);
         $statement->execute();
-    }
-    catch (PDOException $exception) {
-        
+    } catch (PDOException $exception) {
+
         error_log($exception->getMessage());
         $dbh = null;
-        exit;
+        exit();
     }
-    
+
     // Send the message.
-    sendMessage($update['message']['chat']['id'], getTranslation('gym_name_updated'));
+    sendMessage($update['message']['chat']['id'], $message);
 }
 ?>
+        
